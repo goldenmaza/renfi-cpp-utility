75da0f424eda3bacdaaa98ac409847df6c3a8cad
Mats Richard Hellstrand <goldenmaza@outlook.com>
2023-09-14 18:13:25 +0200

    [FileProcessingUtil] Removal of # keywords
    Looks like I added the header definition keywords
    in a CPP file, this has now been removed.

--- src/file_processing_util.cpp
+++ src/file_processing_util.cpp
@@ -3,9 +3,6 @@
  * @version (13th of September, 2023)
  */
 
-#ifndef FILE_PROCESSING_UTIL_CPP_
-#define FILE_PROCESSING_UTIL_CPP_
-
 #include <iostream>
 #include <string>
 #include <sys/stat.h>
@@ -17,5 +14,3 @@ using namespace std;
 bool FileProcessingUtil::validateTarget(struct stat sb, string target) {
 	return stat(target.c_str(), &sb) == 0;
 }
-
-#endif /* FILE_PROCESSING_UTIL_CPP_ */
