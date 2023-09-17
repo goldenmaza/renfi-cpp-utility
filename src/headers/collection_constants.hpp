90817f58e5735fbe4f9780733ea9e16bd2d2cf5d
Mats Richard Hellstrand <goldenmaza@outlook.com>
2023-09-17 12:52:09 +0200

    [Constants] Static keyword introduced
    As the HelpGuideUtil was being implemented, I was
    getting multiple definitions exceptions. This change
    makes it possible to continue with the development as
    all vectors and map are now static.

--- src/headers/collection_constants.hpp
+++ src/headers/collection_constants.hpp
@@ -16,12 +16,12 @@
 using namespace std;
 
 // Application vectors for verifying processing...
-vector<string_view> FLOW_FLAGS {
+static vector<string_view> FLOW_FLAGS {
     FILE_PROCESSING,
     DATA_PROCESSING
 };
 
-vector<string_view> BRANCH_FLAGS {
+static vector<string_view> BRANCH_FLAGS {
 	COMPARE_PROCESSING,
 	CROP_PROCESSING,
 	CONVERT_PROCESSING,
@@ -32,24 +32,24 @@ vector<string_view> BRANCH_FLAGS {
 	LIST_PROCESSING
 };
 
-vector<string_view> RESOURCE_FLAGS {
+static vector<string_view> RESOURCE_FLAGS {
 	IMAGE_PROCESSING,
 	VIDEO_PROCESSING
 };
 
-vector<string_view> TYPE_FLAGS {
+static vector<string_view> TYPE_FLAGS {
 	CREATION_TIME_FLAG,
 	LAST_MODIFIED_TIME_FLAG,
 	LAST_ACCESS_TIME_FLAG
 };
 
-vector<string_view> HELP_FLAGS {
+static vector<string_view> HELP_FLAGS {
 	FULL_HELP_FLAG,
 	HYPHEN_HELP_FLAG,
 	HYPHEN_FULL_HELP_FLAG
 };
 
-map<string_view, vector<string_view>> PROCESSING_SUPPORT {
+static map<string_view, vector<string_view>> PROCESSING_SUPPORT {
 	{IMAGE_PROCESSING, {EXTENSION_JPG, EXTENSION_JPEG, EXTENSION_PNG, EXTENSION_BMP}},
 	{VIDEO_PROCESSING, {EXTENSION_AVI, EXTENSION_MP4, EXTENSION_MOV, EXTENSION_MPG, EXTENSION_WMV, EXTENSION_MKV}}
 };
